7ff5a1c6a50d000d50444bb808c04b07b7d887c0
Steve Hartken <steve.hartken@gmail.com>
2018-12-19 14:20:36 -0500

    Launch -- Info
    Push for updated graphql schema

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,62 @@ import Layout from '../components/Layout'
 import styled from "styled-components"
 
 const Hero = styled.section`
-  min-height: 757px;
-  background-size: cover;
+  height: 805px;
+  background-size: contain;
   background-repeat: no-repeat;
+  background-color: #F6F9F2;
+`
+const Heading = styled.h1`
+  margin-top: 500px;
+  text-align: center;
+  color: white !important;
+  font-family: "Gotham Book";
+  text-transform: uppercase;
+  font-size: 30px;
+  font-weight: 300;`
+
+const HeadingTag = styled.strong`
+  font-size: 60px;
+  font-weight: 700;
+`
+
+const Search = styled.aside`
+  width: 640px;
+  height: 90px;
+  margin: 0 auto;
+  border-radius:46px;
+  box-shadow: 0 4px 4px rgba(29, 134, 73, 0.14);
+  background-color: #ffffff;
+  margin-top: 60px;
+  line-height: 90px;
+  text-align: center;
+`
+
+const SelectWrap = styled.div`
+  vertical-align: middle;
+  width: 180px;
+  height: 50px !important;
+  margin: auto 10px;
+`
+
+const Select = styled.select`
+  height: auto !important;
+  line-height: 50px;
+  height: 48px !important;
+  width: 180px;
+`
+
+const Button = styled.a`
+  font-family: "Gotham Book";
+  vertical-align: middle;
+  margin: auto 10px;
+  width: 200px;
+  height: 50px;
+  font-size: 16px;
+  font-weight: 700;
+`
+const Info = styled.section`
+  height: 757px;
   background-color: #F6F9F2;
 `
 
@@ -29,12 +82,31 @@ export default class IndexPage extends React.Component {
                 }}>
           <div className="container content">
             <div className="column is-10 is-offset-1">
-              <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                {data.frontmatter.heading1}<br></br><strong>{data.frontmatter.heading2}</strong>
-              </h1>
+              <Heading className="title">
+                {data.frontmatter.heading1}
+                <br />
+                <HeadingTag>{data.frontmatter.heading2}</HeadingTag>
+              </Heading>
             </div>
           </div>
+          <Search>
+            <SelectWrap className="select is-rounded">
+              <Select>
+                <option>UAE</option>
+              </Select>
+            </SelectWrap>
+            <SelectWrap className="select is-rounded">
+              <Select>
+                <option>Select City</option>
+              </Select>
+            </SelectWrap>
+            <Button className="button is-link is-rounded">View Golf Course</Button>
+          </Search>
         </Hero>
+
+        <Info>
+          <Img fixed={data.frontmatter.info1} />
+        </Info>
       </Layout>
     )
   }
@@ -78,15 +150,17 @@ query HomePage {
           depth
         }
         frontmatter{
-          title
-          heading
-          description
+          heading1
+          heading2
           image {
             childImageSharp {
               fluid(maxWidth: 2048, quality: 100) {
                 ...GatsbyImageSharpFluid
               }
             }
+          }
+          info1 {
+            
           }
         }
       }
