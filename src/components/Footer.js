dfd76ef37834de1fee87dcd3d05b706ddf63995f
Steve Hartken <steve.hartken@gmail.com>
2018-12-28 10:57:19 -0500

    Footer -- Schema

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import PropTypes from 'prop-types'
-import Select from '../utilities/Select'
+import { Link } from "gatsby"
 
 const FooterWrap = styled.section`
   background-color: #1a428a;
@@ -12,31 +12,65 @@ const FooterWrap = styled.section`
   margin-top:-160px;
 `;
 
+const Menu = link => <li>
+  <Link to={link.href}>{link.text}</Link>
+  {link.children.map( child => <Menu link={child}/>)}
+</li>;
 
-
-const Footer = ({data}) => <FooterWrap>   
+const Footer = ({data}) => {
+console.log(data);  
+return <FooterWrap>   
     <div className="columns">
-        
+        <div className="column one-quater">{<img alt={data.footerLogo.alt} src={
+        !!data.footerLogo.image.childImageSharp
+            ? data.footerLogo.image.childImageSharp.fluid.src
+            : data.footerLogo.image
+        } />}</div>
+        <div className="column one-quarter">
+        <ul>
+        {data.footerColumn1.map( link => <Menu link/> )}
+        {data.footerColumn2.map( link => <Menu link/> )}
+        </ul>
+        </div>
     </div>
-</FooterWrap>
+</FooterWrap>}
 
 export default props => (
     <StaticQuery
       query={graphql`{
-  allMarkdownRemark(filter: {frontmatter: {title: {eq: "Home"}}}) {
-    edges {
-      node {
-        frontmatter {
-          footerLogo
-          footerColumn1
-          footerColumn2
-          footerSocialHeading
-          footerSocial
+        allMarkdownRemark(filter: {frontmatter: {title: {eq: "Home"}}}) {
+          edges {
+            node {
+              frontmatter {
+                footerLogo{
+                  childImageSharp {
+                    fluid(maxWidth: 2048, quality: 100) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+                footerColumn1{
+                  text,
+                  href,
+                  children{
+                    text,
+                    href
+                  }
+                }
+                footerColumn2{
+                  text,
+                  href,
+                }
+                footerSocialHeading
+                footerSocial{
+                  text,
+                  href
+                }
+              }
+            }
+          }
         }
-      }
-    }
-  }
-}`} render={data => <Footer data={data.allMarkdownRemark.edges[0].node.frontmatter} {...props} />} />
+      }`} render={data => <Footer data={data.allMarkdownRemark.edges[0].node.frontmatter} {...props} />} />
 )
 
 Footer.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Hero from '../components/Hero';
 import Infographic from '../components/Info';
 import Featured from '../components/Featured';
 import Recent from '../components/Recent';
+import Footer from '../components/Footer';
 
 export const IndexTemplate = () => <Layout>
   <Hero />
   <Infographic />
   <Featured />
   <Recent />
+  <Footer />
 </Layout>
 
 export default IndexTemplate;
