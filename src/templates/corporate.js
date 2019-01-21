3bb724fc534241c25de45cb499cfb70bbe6c909b
Steve Hartken <steve.hartken@gmail.com>
2019-01-21 10:23:11 -0500

    Scaffolding -- Pages
    Created templates for each page in sketch

---
+++ src/templates/corporate.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export const PageTemplate = ({
+  title,
+}) => (
+  <section className="section section--gradient">
+    <div className="container">
+      Preview Offline
+    </div>
+  </section>
+)
+
+PageTemplate.propTypes = {
+  title: PropTypes.string,
+}
+
+const Post = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <PageTemplate
+        title={frontmatter.title}
+      />
+    </Layout>
+  )
+}
+
+Post.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default Post
+
+export const postQuery = graphql`
+  query Post($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
