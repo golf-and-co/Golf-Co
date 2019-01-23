3e18202e92de4886818c39d978c49f13b50f42eb
David <davidprestonon877@gmail.com>
2019-01-23 11:17:01 +1000

    Package Details -- Template and Schema
    Copied from pacakgeListings for reference
    
    Markdown for page level content

---
+++ src/templates/packageDetails.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import HeroSmall from '../components/HeroSmall';
+import Content from '../components/Content';
+import Listing from '../components/Listing';
+import Footer from '../components/Footer';
+
+export const PageTemplate = ({
+  title,
+}) => (
+  <section className="section section--gradient">
+    <div className="container">
+      Preview Offline
+    </div>
+  </section>
+)
+
+PageTemplate.propTypes = {
+  title: PropTypes.string,
+}
+
+const packageListings = ({ data }) => <Layout>
+    <HeroSmall data={data.packageListingPage.edges[0].node.frontmatter} />
+    <Footer />
+</Layout>;
+
+packageListings.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default packageListings
+
+export const packageListingsQuery = graphql`
+{
+  packageListingPage:allMarkdownRemark(filter: {frontmatter: {title: {eq: "Golf Packages"}}}) {
+    edges {
+      node {
+        frontmatter {
+          title
+          image{
+            childImageSharp{
+              fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          description
+        }
+      }
+    }
+  }
+  courses:allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "packageListings"}}}){
+    edges{
+       node{
+        frontmatter{
+          title
+        }
+      }
+    }
+  }
+}
+`
