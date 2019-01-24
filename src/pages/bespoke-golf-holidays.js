aa800c66aefeb473f5be2a01a5e8a65da4e676d5
Waleed-BS <waleed-bs@live.com>
2019-01-24 02:05:03 -0500

    feat: Add bespoke-golf-holidays page

---
+++ src/pages/bespoke-golf-holidays.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import HeroSmall from '../components/HeroSmall'
+import Infographic from '../components/Info'
+import Featured from '../components/Featured'
+import Recent from '../components/Recent'
+import Footer from '../components/Footer'
+
+export const BespokeGolfHolidays = () => (
+  <Layout>
+    {/* use HeroSmall */}
+    {/* <HeroSmall /> */}
+    <Infographic />
+  </Layout>
+)
+
+export default BespokeGolfHolidays
