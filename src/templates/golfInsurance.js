f83a8345cef7691941524b2189f1e84ec492da38
Steve Hartken <steve.hartken@gmail.com>
2019-02-27 15:36:37 -0500

    Golf Insurance -- Styles and Content
    Fix for infographic images

--- src/templates/golfInsurance.js
+++ src/templates/golfInsurance.js
@@ -3,37 +3,15 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout';
 import HeroSmall from "../components/HeroSmall";
-import styled from 'styled-components'
 import Infographic from '../components/Info';
 import Footer from '../components/Footer';
-
-const Card = styled.section`
-  width: 356px;
-  height: 428px;
-  border-radius: 10px;
-
-  h6 {
-    color: #1d8649;
-    font-size:26px;
-    font-family: "Gotham Bold";
-    line-height: 26px;
-    padding: 25px 30px;
-    text-align: center;
-  }
-
-  p {
-    padding: 10px 15px;
-    text-align: center;
-    font-size: 14px;
-  }
-`;
 
 const Insurance = ({ data }) => { 
 return <Layout>  
   <HeroSmall data={{...data.markdownRemark.frontmatter, backgroundColor:"#f6f9f2", title:false}}  />
   <div style={{backgroundColor:"#f6f9f2"}}>
     <div className="container">
-      <p style={{color: "#4a4a4a"}}>{data.markdownRemark.frontmatter.paragraph1}</p>
+      <p style={{color: "#4a4a4a", paddingTop:"20px", textAlign:"center"}}>{data.markdownRemark.frontmatter.paragraph1}</p>
     </div>
   </div>
   <Infographic data={{...data.markdownRemark.frontmatter, filled:true}} />    
