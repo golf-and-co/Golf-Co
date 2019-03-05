2c80689a714da468326a424b2dd8be40c4a49470
Steve Hartken <steve.hartken@gmail.com>
2019-03-05 14:30:16 -0500

    Buildfix -- Removed mapping

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,4 @@ module.exports = {
     'gatsby-plugin-purgecss', // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-  mapping: {
-    "MarkdownRemark.frontmatter.addOns": "MarkdownRemark",
-  },
 }
