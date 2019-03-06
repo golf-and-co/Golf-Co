f16609039c5b218b7dce3b341166eed79b1e0c35
Steve Hartken <steve.hartken@gmail.com>
2019-03-05 20:28:04 -0500

    Static Pages -- Schema
    Upload schema for page creation

---
+++ src/templates/static.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout';
+import HeroSmall from "../components/HeroSmall";
+import Content from '../components/Content'
+import Footer from '../components/Footer';
+
+const Static = ({ data }) => {
+  // adapter for content
+
+  data.markdownRemark.frontmatter.description = <ReactMarkdown source={data.markdownRemark.frontmatter.body} />;
+
+  return <Layout>
+    <HeroSmall data={{...data.markdownRemark.frontmatter, backgroundColor:"#f5f8f1"}} />
+    <Content data={data.markdownRemark.frontmatter} />
+    <Footer />
+  </Layout>;
+}
+
+Static.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object.isRequired
+    }),
+  }),
+}
+
+export default Static
+
+export const staticQuery = graphql`
+  query static($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      rawMarkdownBody
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        
+      }
+    }
+  }
+`
