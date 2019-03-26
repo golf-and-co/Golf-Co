b344c93b640ad669f0081b2110e3645e2e755d1d
Steve Hartken <steve.hartken@gmail.com>
2019-03-26 18:00:05 -0400

    Contact Page -- Styles
    Styles for contact page
    
    Need to hide for mobile, allow different size columns to fit and show right area code

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -4,8 +4,6 @@ import { graphql } from "gatsby";
 import styled from "styled-components";
 import Layout from "../components/Layout";
 import HeroSmall from "../components/HeroSmall";
-import ReactCountryFlag from "react-country-flag";
-import queryString from "query-string";
 import Footer from "../components/Footer";
 
 const Wrap = styled.div`
@@ -13,7 +11,13 @@ const Wrap = styled.div`
   padding-bottom: 345px;
 
   @media (max-width: 768px) {
-    padding-bottom: 100px;
+    padding: 20px 20px 100px 20px;
+  }
+
+  h6 {
+    color: #5267a3;
+    text-align: center;
+    margin: 20px auto 10px auto;
   }
 
   .container {
@@ -21,10 +25,11 @@ const Wrap = styled.div`
   }
 
   label {
+    display: flex;
+    align-items: center;
     color: #1d8649;
     font-weight: 700;
     text-transform: uppercase;
-    margin-top: 30px;
     font-size: 14px;
   }
 
@@ -50,28 +55,33 @@ const Wrap = styled.div`
   .input.is-rounded {
     margin: 0 auto;
     display: block;
-    max-width: 300px;
     padding: 15px;
+    border: 1px solid #81b997;
+    box-shadow: 1px 1px 1px #cde1cf;
   }
 
-  #phoneWrap {
+  .control {
+    width: 100%;
+  }
+
+  .purpose .column {
     display: flex;
-    justify-content: center;
+    margin-top: 30px;
   }
 
-  #phoneWrap .icon.is-left {
-    left: 6px;
+  .purpose input {
+    margin-right: 10px;
   }
 
-  #countryCode {
-    width: 125px;
-    margin: 0 5px;
-  }
-
-  #phone {
-    width: 165px;
-    border-radius: 20px;
-    margin: 0 10px;
+  button.button {
+    margin: 20px auto;
+    text-align: center;
+    display: block;
+    text-transform: uppercase;
+    padding: 0 50px;
+    height: 3rem;
+    background-color: #1f4388;
+    font-weight: bold;
   }
 
   .textarea.is-rounded {
@@ -83,61 +93,6 @@ const Wrap = styled.div`
       min-width: auto;
     }
   }
-
-  input[type="submit"] {
-    margin: 0 auto;
-    display: block;
-  }
-
-  input[type="date"] {
-    -webkit-appearance: none;
-  }
-
-  input[type="date"]::-ms-clear {
-    display: none;
-  }
-  input[type="date"]::-webkit-inner-spin-button,
-  input[type="date"]::-webkit-clear-button {
-    display: none;
-  }
-
-  .players-wrapper {
-    display: block;
-    width: 150px;
-    margin: 0 auto;
-  }
-
-  .players-wrapper input {
-    width: 150px;
-    padding: 10px 15px;
-    text-align: center;
-  }
-
-  .decrease-button,
-  .increase-button {
-    position: absolute;
-    z-index: 1;
-    border-radius: 15px;
-    box-shadow: none;
-    border: 1px solid #dbdbdb;
-    background: #fff;
-    height: 28px;
-    top: 5px;
-    width: 30px;
-    padding: 0px;
-    font-size: 30px;
-    line-height: 15px;
-    vertical-align: middle;
-    color: #dbdbdb;
-  }
-
-  .decrease-button {
-    left: 5px;
-  }
-
-  .increase-button {
-    right: 5px;
-  }
 `;
 
 const Content = styled.div`
@@ -148,17 +103,6 @@ const Content = styled.div`
   @media (max-width: 768px) {
     padding: 20px 0 0 0;
   }
-`;
-
-const DatePicker = styled.input`
-  border: 1px solid #d9d9d9;
-  width: 210px;
-  height: 40px;
-  padding: 8px;
-  border-radius: 30px;
-  display: block;
-  margin: 0 auto;
-  box-shadow: inset 0 1px 2px rgba(10, 10, 10, 0.1);
 `;
 
 const Contact = ({ data }) => {
@@ -190,6 +134,7 @@ const Contact = ({ data }) => {
                     name="title"
                     className="input is-rounded"
                     type="text"
+                    placeholder="Title"
                   />
                 </div>
               </div>
@@ -199,6 +144,7 @@ const Contact = ({ data }) => {
                     name="firstName"
                     className="input is-rounded"
                     type="text"
+                    placeholder="First Name"
                   />
                 </div>
               </div>
@@ -208,64 +154,162 @@ const Contact = ({ data }) => {
                     name="lastName"
                     className="input is-rounded"
                     type="text"
+                    placeholder="Last Name"
                   />
                 </div>
               </div>
             </div>
 
             <div className="columns">
-              <div className="column">
+              <div className="column is-full">
                 <div className="control">
                   <input
-                    name="title"
+                    name="company"
                     className="input is-rounded"
                     type="text"
+                    placeholder="Company"
+                  />
+                </div>
+              </div>
+            </div>
+            <h6>Your Contact Details</h6>
+            <div className="columns">
+              <div className="column is-full">
+                <div className="control">
+                  <input
+                    name="email"
+                    className="input is-rounded"
+                    type="email"
+                    placeholder="Email"
                   />
                 </div>
               </div>
             </div>
 
-            <div className="columns">
-              <div className="column">
-                <div className="control">
+            <div className="columns is-mobile">
+              <div className="column is-2-desktop is-one-quarter-mobile">
+                <div className="control is-block-desktop is-hidden-mobile">
                   <input
-                    name="title"
+                    name="areaCodeWork"
                     className="input is-rounded"
                     type="text"
+                    placeholder="UAE (+971)"
+                    defaultValue="UAE (+971)"
+                  />
+                </div>
+                <div className="control is-hidden-desktop is-block-mobile">
+                  <input
+                    name="areaCodeWork"
+                    className="input is-rounded"
+                    type="text"
+                    placeholder="+971"
+                    defaultValue="+971"
+                  />
+                </div>
+              </div>
+              <div className="column is-4-desktop is-three-quarters-mobile">
+                <div className="control">
+                  <input
+                    name="workPhone"
+                    className="input is-rounded"
+                    type="text"
+                    placeholder="Work Phone"
+                  />
+                </div>
+              </div>
+              <div className="column is-2-desktop is-one-quarter-mobile is-hidden-mobile is-flex-desktop">
+                <div className="control">
+                  <input
+                    name="areaCodeMobile"
+                    className="input is-rounded"
+                    type="text"
+                    placeholder="UAE (+971)"
+                    defaultValue="UAE (+971)"
+                  />
+                </div>
+              </div>
+              <div className="column is-4-desktop is-three-quarters-mobile is-hidden-mobile is-flex-desktop">
+                <div className="control">
+                  <input
+                    name="workMobile"
+                    className="input is-rounded"
+                    type="text"
+                    placeholder="Mobile Phone"
                   />
                 </div>
               </div>
             </div>
-
-            <div className="columns">
-              <div className="column">
+            <div className="columns is-flex-mobile is-hidden-desktop">
+              <div className="column is-2-desktop is-one-quarter-mobile">
                 <div className="control">
                   <input
-                    name="title"
+                    name="areaCodeMobile"
                     className="input is-rounded"
                     type="text"
+                    placeholder="+971"
+                    defaultValue="+971"
                   />
                 </div>
               </div>
-              <div className="column">
+              <div className="column is-4-desktop is-three-quarters-mobile">
                 <div className="control">
                   <input
-                    name="firstName"
+                    name="workMobile"
                     className="input is-rounded"
                     type="text"
-                  />
-                </div>
-              </div>
-              <div className="column">
-                <div className="control">
-                  <input
-                    name="lastName"
-                    className="input is-rounded"
-                    type="text"
+                    placeholder="Mobile Phone"
                   />
                 </div>
               </div>
             </div>
+            <h6>What is the Primary Purpose of the Event</h6>
+            <div className="columns purpose">
+              <div className="column is-quarter">
+                <input
+                  type="radio"
+                  id="advertising"
+                  name="purpose"
+                  value="advertising"
+                  checked
+                />
+                <label for="advertising">Advertising / Promoting Brand</label>
+              </div>
+              <div className="column is-quarter">
+                <input
+                  type="radio"
+                  id="clients"
+                  name="purpose"
+                  value="clients"
+                  checked
+                />
+                <label for="clients">
+                  Relationship Building with Clients and Staff
+                </label>
+              </div>
+              <div className="column is-quarter">
+                <input
+                  type="radio"
+                  id="launch"
+                  name="purpose"
+                  value="launch"
+                  checked
+                />
+                <label for="launch">Product Launch</label>
+              </div>
+              <div className="column is-quarter">
+                <input
+                  type="radio"
+                  id="charity"
+                  name="purpose"
+                  value="charity"
+                  checked
+                />
+                <label for="charity">Charity Day</label>
+              </div>
+            </div>
+            <button className="button is-rounded is-link" name="submit">
+              Submit
+            </button>
           </form>
         </div>
       </Wrap>
