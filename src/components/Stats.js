babe329ed3829a0a3dcefa4f2a7011344a89a11b
Steve Hartken <steve.hartken@gmail.com>
2019-04-06 15:25:27 -0400

    Mobile Check
    Hide n+3 stats for mobile to prevent overflow
    
    Hid stats button until share provider, like addthis is selected

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { v4 } from 'uuid'
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { v4 } from "uuid";
 
 const Section = styled.section`
   background-color: #81aa8c;
@@ -12,20 +12,20 @@ const Section = styled.section`
     margin: 15px auto;
     display: block;
   }
-`
+`;
 
 const List = styled.ul`
   display: flex;
   justify-content: flex-start;
   padding: 0 0 20px 0;
-`
+`;
 
 const Item = styled.li`
   background-color: #81aa8c;
   border-right: 1px dashed #000;
   padding: 1px 25px;
   text-align: center;
-  
+
   img {
     height: 23px;
   }
@@ -33,62 +33,81 @@ const Item = styled.li`
   &:last-child {
     border-right: none !important;
   }
-`
+  @media (max-width: 768px) {
+    & {
+      display: none;
+    }
+    &:first-child,
+    &:nth-child(2),
+    &:nth-child(3) {
+      display: block;
+    }
+  }
+`;
 
 const Label = styled.h6`
   text-transform: uppercase;
-`
+`;
 
 const Value = styled.span`
   text-transform: uppercase;
-`
+`;
 
 const StatItem = ({ data, style }) => {
   data.label = data.label.split(" ");
   data.label = data.label[0];
-  return <Item style={style}>
-    <img id="image" src={data.icon.publicURL} alt={data.label} />
-    <Label>{data.label}</Label>
-    <Value>{data.value}</Value>
-  </Item>
-}
+  return (
+    <Item style={style}>
+      <img id="image" src={data.icon.publicURL} alt={data.label} />
+      <Label>{data.label}</Label>
+      <Value>{data.value}</Value>
+    </Item>
+  );
+};
 
 const StatList = ({ data, fields, slug, hideStats, center, button }) => {
-  if(hideStats) return <span />;
+  if (hideStats) return <span />;
 
   const listStyle = {};
   const itemStyle = {};
-  if(center) {
-    listStyle.justifyContent="center";
-  }
-  else {
+  if (center) {
+    listStyle.justifyContent = "center";
+  } else {
     // for cards
-    data.stats = data.stats.slice(0,3);
+    data.stats = data.stats.slice(0, 3);
     itemStyle.width = "86px";
-    itemStyle.padding ="10px";
+    itemStyle.padding = "10px";
   }
 
   let Button = () => {
-    if(button) {
-      return <button onClick={() => window.location = fields.slug}  className="button is-success is-rounded">View Course Details</button>
+    if (button) {
+      return (
+        <button
+          onClick={() => (window.location = fields.slug)}
+          className="button is-success is-rounded"
+        >
+          View Course Details
+        </button>
+      );
+    } else {
+      return <span />;
     }
-    else {
-      return <span />
-    }
-  }
+  };
 
-  return <Section onClick={() => window.location = slug} id="stats">
-    <List style={listStyle}>
-      {data.stats.map(stat => (
-        <StatItem data={stat} key={v4()} style={itemStyle}/>
-      ))}
-    </List>
-    <Button />
-  </Section>
-}
+  return (
+    <Section id="stats">
+      <List style={listStyle}>
+        {data.stats.map(stat => (
+          <StatItem data={stat} key={v4()} style={itemStyle} />
+        ))}
+      </List>
+      <Button />
+    </Section>
+  );
+};
 
-export default StatList
+export default StatList;
 
 StatList.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
