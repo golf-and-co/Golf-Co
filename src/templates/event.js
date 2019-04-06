1bb584a41104f9e2342680423b6fb469f434ff11
Steve Hartken <steve.hartken@gmail.com>
2019-04-06 15:49:46 -0400

    Mobile styles for event page

--- src/templates/event.js
+++ src/templates/event.js
@@ -26,13 +26,17 @@ const Wrap = styled.div`
   .slick-track .slick-slide {
     display: flex;
     height: auto;
-    align-items: center;
+    align-items: baseline;
     justify-content: center;
   }
 
   @media (min-width: 768px) {
     p {
       padding: 0 20px;
+    }
+
+    .slick-track .slick-slide {
+      align-items: baseline;
     }
   }
 `;
@@ -59,125 +63,131 @@ const galleryDetails = ({ data }) => {
           showTitle: false
         }}
       />
-      <p
-        style={{
-          paddingTop: "30px",
-          textAlign: "center",
-          backgroundColor: "#E4ECD9",
-          fontWeight: "bold",
-          color: "#000"
-        }}
-      >
-        {data.markdownRemark.frontmatter.title}
-      </p>
-      <p
-        style={{
-          textTransform: "uppercase",
-          textAlign: "center",
-          backgroundColor: "#E4ECD9",
-          fontWeight: "bold",
-          color: "#000"
-        }}
-      >
-        {data.markdownRemark.frontmatter.date}
-      </p>
-      <div style={{ paddingTop: "25px", backgroundColor: "#E4ECD9" }}>
-        <div className="container">
-          <p style={{ color: "#000" }}>
-            {data.markdownRemark.frontmatter.description}
-          </p>
-        </div>
-      </div>
-      <div style={{ paddingBottom: "200px", backgroundColor: "#E4ECD9" }}>
-        <Wrap className="container">
-          <div style={{ display: displayGallery }}>
-            <button
-              onClick={() => setGallery(false)}
-              className="is-rounded "
-              style={{
-                backgroundColor: "#1d8649",
-                color: "#ffffff",
-                fontFamily: "Gotham Book",
-                fontSize: "1rem",
-                fontWeight: "300",
-                textTransform: "uppercase",
-                position: "relative",
-                padding: "5px 10px",
-                margin: "10px 0px",
-                letterSpacing: ".1px",
-                textAlign: "center",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer"
-              }}
-            >
-              Back
-            </button>
-            {data.markdownRemark.frontmatter.albums.map(entry => {
-              let displayValue;
-              if (gallery === entry.name) {
-                displayValue = "block";
-              } else {
-                displayValue = "none";
-              }
-
-              return (
-                <div style={{ display: displayValue }}>
-                  <Slider
-                    key={v4()}
-                    {...{
-                      dots: true,
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                      autoplay: true,
-                      autoplaySpeed: 2000
-                    }}
-                  >
-                    {entry.images.map(image => (
-                      <img
-                        key={v4()}
-                        alt={entry.name}
-                        src={image.image.replace("../../../static", "")}
-                      />
-                    ))}
-                  </Slider>
-                </div>
-              );
-            })}
+      <div style={{ padding: "0 10px", backgroundColor: "#E4ECD9" }}>
+        <p
+          style={{
+            paddingTop: "30px",
+            textAlign: "center",
+            backgroundColor: "#E4ECD9",
+            fontWeight: "bold",
+            color: "#000"
+          }}
+        >
+          {data.markdownRemark.frontmatter.title}
+        </p>
+        <p
+          style={{
+            textTransform: "uppercase",
+            textAlign: "center",
+            backgroundColor: "#E4ECD9",
+            fontWeight: "bold",
+            color: "#000"
+          }}
+        >
+          {data.markdownRemark.frontmatter.date}
+        </p>
+        <div style={{ paddingTop: "25px", backgroundColor: "#E4ECD9" }}>
+          <div className="container">
+            <p style={{ color: "#000" }}>
+              {data.markdownRemark.frontmatter.description}
+            </p>
           </div>
-
-          <div style={{ display: displayAlbums }}>
-            <Slider
-              {...{
-                dots: true,
-                slidesToShow: parseInt(
-                  data.markdownRemark.frontmatter.imagesPerSlide
-                ),
-                slidesToScroll: parseInt(
-                  data.markdownRemark.frontmatter.imagesPerSlide
-                ),
-                infinite: true
-              }}
-            >
+        </div>
+        <div style={{ paddingBottom: "200px", backgroundColor: "#E4ECD9" }}>
+          <Wrap className="container">
+            <div style={{ display: displayGallery }}>
+              <button
+                onClick={() => setGallery(false)}
+                className="is-rounded "
+                style={{
+                  backgroundColor: "#1d8649",
+                  color: "#ffffff",
+                  fontFamily: "Gotham Book",
+                  fontSize: "1rem",
+                  fontWeight: "300",
+                  textTransform: "uppercase",
+                  position: "relative",
+                  padding: "5px 10px",
+                  margin: "10px 0px",
+                  letterSpacing: ".1px",
+                  textAlign: "center",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer"
+                }}
+              >
+                Back
+              </button>
               {data.markdownRemark.frontmatter.albums.map(entry => {
+                let displayValue;
+                if (gallery === entry.name) {
+                  displayValue = "block";
+                } else {
+                  displayValue = "none";
+                }
+
                 return (
-                  <div key={v4()} onClick={() => setGallery(entry.name)}>
-                    <img
-                      src={entry.images[0].image.replace("../../../static", "")}
-                      alt="Gallery"
-                    />
-                    <div style={{ color: "#000", textAlign: "center" }}>
-                      {entry.date}
-                    </div>
-                    <div style={{ color: "#555", textAlign: "center" }}>
-                      {entry.name}
-                    </div>
+                  <div style={{ display: displayValue }}>
+                    <Slider
+                      key={v4()}
+                      {...{
+                        dots: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        autoplay: true,
+                        autoplaySpeed: 2000
+                      }}
+                    >
+                      {entry.images.map(image => (
+                        <img
+                          key={v4()}
+                          alt={entry.name}
+                          src={image.image.replace("../../../static", "")}
+                        />
+                      ))}
+                    </Slider>
                   </div>
                 );
               })}
-            </Slider>
-          </div>
-        </Wrap>
+            </div>
+
+            <div style={{ display: displayAlbums }}>
+              <Slider
+                {...{
+                  dots: true,
+                  slidesToShow: parseInt(
+                    data.markdownRemark.frontmatter.imagesPerSlide
+                  ),
+                  slidesToScroll: parseInt(
+                    data.markdownRemark.frontmatter.imagesPerSlide
+                  ),
+                  infinite: true
+                }}
+              >
+                {data.markdownRemark.frontmatter.albums.map(entry => {
+                  return (
+                    <div key={v4()} onClick={() => setGallery(entry.name)}>
+                      <img
+                        style={{ cursor: "pointer" }}
+                        src={entry.images[0].image.replace(
+                          "../../../static",
+                          ""
+                        )}
+                        alt="Gallery"
+                      />
+                      <div style={{ color: "#000", textAlign: "center" }}>
+                        {entry.date}
+                      </div>
+                      <div style={{ color: "#555", textAlign: "center" }}>
+                        {entry.name}
+                      </div>
+                    </div>
+                  );
+                })}
+              </Slider>
+            </div>
+          </Wrap>
+        </div>
       </div>
       <Footer />
     </Layout>
